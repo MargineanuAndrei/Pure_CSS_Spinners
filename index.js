e0ee5695bcfad3ab2cc927a5def26c0865d6f364
MargineanuAndrei <margineanuandrei94@gmail.com>
2018-05-14 11:14:47 +0300

    change connent of header

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const displayText = () => {
 	element.style.display = "block";
 }
 
+const hideSpinner = () => {
+	element = document.getElementById("spinner");
+	element.style.display = "none";
+}
+
+const changeHeader = () => {
+	element = document.getElementById("header");
+	element.innerHTML = "Goodbye spinner, Hello text!";
+}
+
 setTimeout(function() {
+	hideSpinner();
+	changeHeader();
 	displayText();
 }, 3000);
